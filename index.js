9228af1a467d122b651cad56b05dc9537e277d9e
AliakseiMelnikau <aliaksei.melnikau@gmail.com>
2024-04-10 04:28:02 +0000

    Create test page in DB in Notion

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
 const dotenv = require('dotenv');
+const axios = require('axios')
+const { Client } = require('@notionhq/client')
+
 dotenv.config();
-console.log(process.env.NOTION_API);
+
+const notion = new Client({auth: process.env.NOTION_API_KEY});
+
+async function createNotionPage() {
+  
+
+      const data = {
+        "parent": {
+          "type": "database_id",
+          "database_id": process.env.NOTION_DATABASE_ID
+        }, 
+        "properties": {
+          "Name": {
+            "title": [
+              {
+                "text": {
+                  "content": "Test Name"
+                }
+              }
+            ]
+          },
+
+        }
+      }
+      
+      console.log(`Sending data to Notion`)
+      const response = await notion.pages.create( data )
+      console.log(response)
+}
+
+createNotionPage();
+
+console.log(process.env.NOTION_API_KEY);
